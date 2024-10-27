ea30af47e0babca3a67cf71ff9e330ffe79ab414
Ahmed Mohamud <60895387+FourierTransform1@users.noreply.github.com>
2024-10-27 19:35:42 +0200

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,3 @@
 print("Hello world!")
-<<<<<<< HEAD
 print("Hello darkness my old friend")
 print("I've come to talk with you again")
-=======
-
-print("Hello once again")
->>>>>>> 1d609b5d69230997d0ec535c757244495103ddfa
